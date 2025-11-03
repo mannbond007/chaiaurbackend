c0df2beea4203b29bef5658628a7209aed855a99
sysrover007 <thelearning0007@gmail.com>
2025-11-03 23:11:52 +0530

    utils coded

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import dotenv from "dotenv";
 dotenv.config({
     path: "./env"
 });
-
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
 
-connectDB().then(() => console.log("DB connected"));
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB connection failed", error);
+    process.exit(1);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,12 @@
+import { Promise } from "mongoose"
+
+const asyncHandler = (requestHandler) => {
+    (req, res , next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+
+
+export { asyncHandler }
+
